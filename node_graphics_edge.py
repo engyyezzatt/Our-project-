157d4e69a22cf25a0a853b4557b464d67a69468b
Engy Ezzat <engyyezzatt@gmail.com>
2022-11-08 16:32:14 +0200

    Edges Finished: Drawing the actual edges between sockets

--- node_graphics_edge.py
+++ node_graphics_edge.py
@@ -1,6 +1,10 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+from Node_Socket import *
+import math
+
+EDGE_CP_ROUNDNESS = 100
 
 
 class GraphicsEdge(QGraphicsPathItem):
@@ -9,12 +13,16 @@ class GraphicsEdge(QGraphicsPathItem):
 
         self.edge = edge
 
-        self._color = QColor("Red")#001000
+        self._color = QColor("Red")  # 001000
         self._pen = QPen(self._color)
-        self._pen.setWidthF(2.0)
 
         self._color_selected = QColor("#00ff00")
         self._pen_selected = QPen(self._color_selected)
+        self._pen_dragging = QPen(self._color)
+        self._pen_dragging.setStyle(Qt.DashLine)
+
+        self._pen.setWidthF(2.0)
+        self._pen_dragging.setWidthF(2.0)
         self._pen_selected.setWidthF(2.0)
 
         self.setFlag(QGraphicsItem.ItemIsSelectable)
@@ -29,13 +37,13 @@ class GraphicsEdge(QGraphicsPathItem):
     def setDestination(self, x, y):
         self.posDestination = [x, y]
 
-
     def paint(self, painter, QStyleOptionGraphicsItem, widget=None):
         self.calcPath()
 
-        painter.setPen(self._pen
-                       if not self.isSelected()
-                       else self._pen_selected)
+        if self.edge.end_socket is None:
+            painter.setPen(self._pen_dragging)
+        else:
+            painter.setPen(self._pen if not self.isSelected() else self._pen_selected)
         painter.setBrush(Qt.NoBrush)
         painter.drawPath(self.path())
 
@@ -57,8 +65,31 @@ class GraphicsEdgeBezier(GraphicsEdge):
         d = self.posDestination
         dist = (d[0] - s[0]) * 0.5
         if s[0] > d[0]: dist *= -1
+        # control point x for source and for destination
+        cpx_s = +dist
+        cpx_d = -dist
+        cpy_s = 0
+        cpy_d = 0
+
+        start_socketpos = self.edge.start_socket.position
+
+        if s[0] > d[0] and start_socketpos in (RIGHT_TOP, RIGHT_BOTTOM) or \
+                (s[0] < d[0] and start_socketpos in (LEFT_BOTTOM, LEFT_TOP)):
+            cpx_d * -1
+            cpx_s * -1
+
+            ## can be removed
+            # cpy_d = (
+            #         s[1] - d[1] / math.fabs(
+            #     (s[1] - d[1]) if (s[1] - d[1]) != 0 else 0.000001)
+            # ) * EDGE_CP_ROUNDNESS
+            #
+            # cpy_s = (
+            #                 d[1] - s[1] / math.fabs(
+            #             (d[1] - s[1]) if (d[1] - s[1]) != 0 else 0.000001)
+            #         ) * EDGE_CP_ROUNDNESS
 
         path = QPainterPath(QPointF(self.posSource[0], self.posSource[1]))
-        path.cubicTo( s[0] + dist, s[1], d[0] - dist, d[1], self.posDestination[0], self.posDestination[1])
+        path.cubicTo(s[0] + cpx_s, s[1] + cpy_s, d[0] + cpx_d, d[1] + cpy_d, self.posDestination[0],
+                     self.posDestination[1])
         self.setPath(path)
-
