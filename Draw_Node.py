b7f4c48b7be194bf85688e4321a80a24e626fe9a
Engy Ezzat <engyyezzatt@gmail.com>
2022-11-09 06:24:03 +0200

    Serilization added

--- Draw_Node.py
+++ Draw_Node.py
@@ -11,15 +11,12 @@ class Node(Serializable):
         super().__init__()
         if outputs is None:
             outputs = []
-
-        self._title = title
         self.scene = scene
 
-
+        self.title = title
 
         self.content = NodeContent(self)
         self.grNode = GraphicsNode(self)
-        self.title = title
 
         self.scene.addNode(self)
         self.scene.myGrScene.addItem(self.grNode)
@@ -50,16 +47,6 @@ class Node(Serializable):
     def setpos(self, x, y):
         self.grNode.setPos(x,y)
 
-    @property
-    # title of this `Node` (getter: current Graphics Node title) (setter: stores and make visible the new title)
-    def title(self):
-        return self._title
-
-    @title.setter
-    def title(self, value):
-        self._title = value
-        self.grNode.title = self._title
-
     def getSocketPosition(self, index, position):
         x = 0 if (position in (LEFT_TOP, LEFT_BOTTOM)) else self.grNode.width
 
@@ -72,7 +59,7 @@ class Node(Serializable):
 
         return [x, y]
 
-        # 3shan.n7rk el edges m3 el sockets with one edge only not multiple
+        # 3shan n7rk el edges m3 el sockets with one edge only not multiple
     def updateConnectedEdges(self):
         for socket in self.inputs + self.outputs:
             if socket.hasEdge():
@@ -107,33 +94,5 @@ class Node(Serializable):
             ('content', self.content.serialize()),
         ])
 
-
     def deserialize(self, data, hashmap={}):
-        self.id = data['id']
-        hashmap[data['id']] = self
-
-        self.setpos(data['pos_x'], data['pos_y'])
-
-        self.title =data['title']
-
-        # go through the inputs and the outputs in json file
-        data['inputs'].sort(key= lambda socket : socket['index'] + socket['position'] * 10000 )
-        data['outputs'].sort(key=lambda socket: socket['index'] + socket['position'] * 10000)
-
-        self.inputs = []
-        for socket_data in data['inputs']:
-            new_socket = Socket(node= self , index= socket_data['index'], position=socket_data['position'],
-                                socket_type=socket_data['socket_type'])
-            new_socket.deserialize(socket_data,hashmap)
-            self.inputs.append(new_socket)
-
-        self.outputs = []
-        for socket_data in data['outputs']:
-            new_socket = Socket(node=self, index=socket_data['index'], position=socket_data['position'],
-                                socket_type=socket_data['socket_type'])
-            new_socket.deserialize(socket_data, hashmap)
-            self.outputs.append(new_socket)
-
-        print(hashmap)
-
-        return True
+        return False
--- Node_Socket.py
+++ Node_Socket.py
@@ -48,8 +48,4 @@ class Socket(Serializable):
         ])
 
     def deserialize(self, data, hashmap={}):
-        self.id = data['id']
-        hashmap[data['id']] = self
-
-        return True
-
+        return False
